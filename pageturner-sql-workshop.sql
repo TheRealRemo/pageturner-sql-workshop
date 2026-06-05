f2e6c951d59e526ed63fde318c84fdb9cedf62f8
TheRealRemo <82535413+TheRealRemo@users.noreply.github.com>
2026-06-05 15:07:46 -0400

    Add files via upload
    completed up to question 19

--- pageturner-sql-workshop.sql
+++ pageturner-sql-workshop.sql
@@ -42,7 +42,7 @@ FROM books b
 JOIN genres ON b.genre_id = genres.genre_ID
 GROUP BY b.genre_ID;
 -- Question 12
-SELECT genres.name, SUM(stock_quantity)
+SELECT genres.name AS GenreNames, SUM(stock_quantity) AS TotalStockQuantity
 FROM books b
 JOIN genres ON b.genre_id = genres.genre_ID
 GROUP BY b.genre_id;
@@ -56,4 +56,26 @@ HAVING COUNT(b.genre_id) > 3;
 SELECT orders.status, COUNT(orders.status) AS StatusCount
 FROM orders
 GROUP BY orders.status;
+-- Question 15
+SELECT books.title, CONCAT(first_name, " ", last_name) AS full_name
+FROM books
+JOIN authors ON books.author_id = authors.author_id;
+-- Question 16
+SELECT books.title BookTitle, genres.name GenreName
+FROM books 
+JOIN genres ON books.genre_id = genres.genre_ID;
+-- Question 17
+SELECT orders.order_id AS Order_ID, order_date, CONCAT(first_name, " ", last_name) AS full_name
+FROM orders
+JOIN customers ON orders.order_id = customers.customer_id;
+-- Question 18
+SELECT books.title order_item_book_title, order_items.quantity, order_items.unit_price
+FROM order_items
+JOIN books ON order_items.book_id = books.book_id;  
+--  Question 19
+SELECT CONCAT(authors.first_name, " ", authors.last_name) AS full_name, count(books.title) AS books_written
+FROM authors
+LEFT JOIN books ON authors.author_id = books.author_id
+GROUP BY full_name;
+
 
