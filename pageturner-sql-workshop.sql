eba31bc5e999a9134fdec8b0b6fe99ea19474826
TheRealRemo <82535413+TheRealRemo@users.noreply.github.com>
2026-06-10 07:49:44 -0400

    Add files via upload
    Completed all questions

--- pageturner-sql-workshop.sql
+++ pageturner-sql-workshop.sql
@@ -84,3 +84,70 @@ FROM customers
 LEFT JOIN orders ON customers.customer_id = orders.customer_id
 GROUP BY full_name
 ORDER BY total_orders DESC;
+-- Question 21
+SELECT books.title, round(avg(reviews.rating), 1) AS rating_average
+FROM books
+JOIN reviews ON books.book_id = reviews.book_id
+GROUP BY books.title
+HAVING COUNT(reviews.rating) >= 1;
+-- Question 22
+SELECT title, price
+FROM books
+WHERE price > (SELECT avg(price) FROM books);
+-- Question 23
+SELECT *
+FROM books
+WHERE books.book_id NOT IN (SELECT order_items.book_id
+FROM order_items);
+-- Question 24
+SELECT *
+FROM customers
+WHERE customer_id NOT IN (SELECT orders.customer_id
+FROM orders);
+-- Question 25
+SELECT title, COUNT(reviews.book_id) AS review_amount
+FROM books
+LEFT JOIN reviews ON reviews.book_id = books.book_id
+GROUP BY books.book_id
+ORDER BY review_amount DESC
+LIMIT 1;
+-- Question 26
+SELECT books.title, SUM(order_items.quantity) AS quantity_count
+FROM books
+LEFT JOIN order_items ON order_items.book_id = books.book_id
+GROUP BY books.title
+ORDER BY quantity_count DESC
+LIMIT 1;
+-- Question 27
+SELECT genres.name, SUM(order_items.quantity * books.price) AS total_revenue
+FROM genres
+JOIN books ON books.genre_id = genres.genre_id 
+JOIN order_items ON order_items.book_id = books.book_id
+GROUP BY genres.name
+ORDER BY total_revenue DESC;
+-- Question 28 (Customers who never ordered omitted)
+SELECT CONCAT(customers.first_name, " ", customers.last_name) AS full_name, 
+SUM(order_items.quantity * books.price) AS total_spent
+FROM customers
+JOIN orders ON orders.customer_id = customers.customer_id
+JOIN order_items ON order_items.order_id = orders.order_id
+JOIN books ON books.book_id = order_items.book_id
+GROUP BY customers.customer_id
+ORDER BY total_spent DESC;
+-- Question 29
+SELECT CONCAT(authors.first_name, " ", authors.last_name) AS full_name, AVG(reviews.rating) AS avg_rating
+FROM authors
+JOIN books ON books.author_id = authors.author_id
+JOIN reviews ON reviews.book_id = books.book_id
+GROUP BY authors.author_id
+ORDER BY avg_rating DESC
+LIMIT 1;
+-- Question 30
+SELECT genres.name, books.title, books.price
+FROM books
+JOIN genres ON genres.genre_id = books.genre_id
+WHERE books.price = (
+SELECT MAX(b.price)
+FROM books b
+WHERE b.genre_id = books.genre_id
+);
